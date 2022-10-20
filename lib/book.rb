ad362226736a894d1f705b8d49e13ffdd79672b8
stephenonchieku1 <onchiekustephen901@gmail.com>
2022-10-20 18:53:06 +0300

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
+class Book
+    attr_reader :title
+    attr_accessor:author_name,:page_count,:genre
+    
 
+    def initialize(title)
+        @title = title
+    end
+    def turn_page
+        puts "page turned"
+    end
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
 # Make your shoe class here!
+class Shoe
+    attr_reader :brand
+    attr_accessor :color, :size, :material, :condition
+    
+    def initialize(brand)
+        @brand = brand
+    end
+    def cobble
+        puts "the shoe has been repaired"
+        @condition = "new"
+    end
+
+end
